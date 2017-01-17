b3de4859f4d11c4dada1b1159c28b976607ed895
rumenNikodimov <ru33@abv.bg>
2017-01-17 14:51:53 +0200

    solved

---
+++ Exam 18.12.2016/Problem 01. Distance/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var speed = int.Parse(Console.ReadLine());
+            var firstTimeInMinutes = int.Parse(Console.ReadLine());
+            var secondTimeInMinutes = int.Parse(Console.ReadLine());
+            var thirdTimeInMinutes = int.Parse(Console.ReadLine());
+
+            
+                      
+            double time1 = firstTimeInMinutes / 60.00;
+            double time2 = secondTimeInMinutes / 60.00;
+            double time3 = thirdTimeInMinutes / 60.00;
+            var speed2 = speed * 1.10;
+
+            var distance1 = time1 * speed;
+            var distance2 = time2   * speed2;
+            var distance3 = time3 * (speed2 * 0.95);
+            var distance = distance3 + distance2 + distance1;
+
+            Console.WriteLine("{0:f2}", distance);
+
+
+
+
+
+
+
+            //razstoqnie = averageSpeed * thirdTimeInMinutes;
+            //Console.WriteLine("{0}", razstoqnie);
+
+        }
+    }
+}
