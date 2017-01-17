4669abc0f6577df51b38db10f4e90eb25d14aaa5
Rumen Tzvetanov Nikodimov <ru33@abv.bg>
2017-01-17 18:39:44 +0200

    Add files via upload

---
+++ Exam 18.12.2016/05. Christmas Hat/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(new string('.', 2 * n - 1) + new string('/', 1) + new string('|', 1) + new string('\\', 1) + new string('.', 2 * n - 1));
+            Console.WriteLine(new string('.', 2 * n - 1) + new string('\\', 1) + new string('|', 1) + new string('/', 1) + new string('.', 2 * n - 1));
+            Console.WriteLine(new string('.', 2 * n - 1) + new string('*', 3) + new string('.', 2 * n - 1));
+
+            for (int i = 0; i < n * 2 - 1; i++)
+            {
+                Console.WriteLine(new string('.', 2 * n - 2 - i) + new string('*', 1) + new string('-', 1 + i) + new string('*', 1) + new string('-', 1 + i) + new string('*', 1) + new string('.', 2 * n - 2 - i));
+            }
+
+            Console.WriteLine(new string('*', 4 * n + 1));
+            Console.Write((String.Concat(Enumerable.Repeat("*.", 2*n))));
+            Console.WriteLine(new string('*', 1));
+            Console.WriteLine(new string('*', 4 * n + 1));
+
+
+        }
+    }
+
+}
